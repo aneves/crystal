eb23556713eb14251296464b6abf5da41685fae5
Andre Neves <drakferion@gmail.com>
2012-12-25 19:39:57 +0100

    Swapping colours.

--- crystal.js
+++ crystal.js
@@ -41,6 +41,15 @@
 					colour = crystal.settings.colours[index];
 				return colour;
 			},
+			_swap: function(one, other) {
+				var a = one.dataset,
+					b = other.dataset,
+					acolor = a.color;
+				a.color = b.color;
+				b.color = acolor;
+				one.style["backgroundColor"] = one.dataset.color;
+				other.style["backgroundColor"] = other.dataset.color;
+			},
 			init: function() {
 				if(!window.console) {
 					window.console = {};
@@ -50,6 +59,10 @@
 					};
 				}
 				var table = document.createElement("table");
+				if(!table.dataset) {
+					crystal.log("This browser does not support dataset.");
+					return;
+				}
 				document.getElementById('board').appendChild(table);
 				crystal.objs.board = table;
 				var row, col, tr, td;
@@ -59,18 +72,30 @@
 						td = document.createElement('td');
 						var colour = crystal.getRandomColour();
 						td.setAttribute('style', 'background-color: ' + colour + ';');
-						td.setAttribute('data-color', colour);
-						td.setAttribute('data-row', row);
-						td.setAttribute('data-col', col);
+						td.dataset.color = colour;
+						td.dataset.row = row;
+						td.dataset.col = col;
 						tr.appendChild(td);
 					}
 					table.appendChild(tr);
 				}
 				crystal.log('Initialized board of ' + crystal.settings.width + "x" + crystal.settings.height + ".");
 				$('#board').on('click', 'td', function() {
-					var row = this.getAttribute('data-row'),
-						col = this.getAttribute('data-col');
-					crystal.log('clicked [' + row + '-' + col + ']');
+					var row = this.dataset.row,
+						col = this.dataset.col,
+						$this = $(this),
+						previous = crystal.objs.selected || null;
+					if(previous === null) {
+						$this.addClass('selected');
+						crystal.objs.selected = this;
+					} else{
+						if(previous !== this) {
+							crystal._swap(previous, this);
+							crystal.objs.selected = null;
+						}
+						$(previous).removeClass('selected');
+						crystal.objs.selected = null;
+					}
 				});
 			}
 		};
